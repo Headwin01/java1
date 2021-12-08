fc0e9af8307d0e5b5eb57679e4c86efca043b70c
Headwin01 <75309190+Headwin01@users.noreply.github.com>
2021-12-09 00:57:26 +0200

    HW-1

---
+++ task2_5.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class task_02_05 {
+	public static void main(String[] args) {
+	    for( int i = args.length - 1 ; i >= 0 ; i--)
+	        System.out.println("Reverse : " + args[i]);
+	}
+
+}
